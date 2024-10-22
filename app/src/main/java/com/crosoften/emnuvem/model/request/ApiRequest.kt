cb0fa94400561ccef4977ab1e2e33055b5191eb8
edijanio <edijanio.souza@hotmail.com>
2024-10-22 11:42:32 -0300

    :recycle: refactor: Injeção de dependencia e arquitetura

--- app/src/main/java/com/crosoften/emnuvem/model/request/ApiRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.crosoften.emnuvem.model.request
-
-import com.crosoften.emnuvem.retrofit.RetrofitConfig
-
-class ApiRequest {
-    fun <S> getService(service: Class<S>): S {
-        return RetrofitConfig.createService(service)
-    }
-}
